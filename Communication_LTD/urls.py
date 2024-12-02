d429e625c95929d6b54102a20a002d2b81073cfd
Yuval-Betito <yuval36811@gmail.com>
2024-12-02 13:42:02 +0200

    Update urls.py

--- Communication_LTD/urls.py
+++ Communication_LTD/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from users import views  # הוספתי את views של האפליקציה users
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -9,4 +10,7 @@ urlpatterns = [
     # Password change paths
     path('password_change/', auth_views.PasswordChangeView.as_view(template_name='users/password_change.html'), name='password_change'),
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'), name='password_change_done'),
+
+    # Add home page route
+    path('', views.home, name='home'),  # הגדרת נתיב לדף הבית
 ]
